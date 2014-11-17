231e0315c79847828ffdb59822a9cd1249d709ce
Lsong <song940@gmail.com>
2014-11-17 16:45:56 +0800

    convert tab to space

--- engine.js
+++ engine.js
@@ -1,35 +1,35 @@
-var fs					=	require('fs');
-var cron 				= require('cron');
-var path 				= require('path');
-var async 			= require('async');
-var redis 			= require("redis");
-var request 		= require('request');
-var Promise			=	require('promise');
-var mongoose		= require('mongoose');
+var fs          = require('fs');
+var cron        = require('cron');
+var path        = require('path');
+var async       = require('async');
+var redis       = require("redis");
+var request     = require('request');
+var Promise     = require('promise');
+var mongoose    = require('mongoose');
 var ProgressBar = require('progress');
-var _						= require('underscore');
+var _           = require('underscore');
 
 require('colors');
 
 var Engine = function(options){
-	this.configs = _.extend({
-		config 	: './config/config',
-		crontab	: './config/crontab',
-		tasks		: './tasks',
-		filters : './filters'
-	}, options);
+  this.configs = _.extend({
+    config  : './config/config',
+    crontab : './config/crontab',
+    tasks   : './tasks',
+    filters : './filters'
+  }, options);
 
-	this.root = __dirname;
+  this.root = __dirname;
 
-	this.tasks = {};
-	this.filters = {};
-	this.crontab = {};
+  this.tasks = {};
+  this.filters = {};
+  this.crontab = {};
 
-	this.load();
+  this.load();
 };
 
 ([ 'running', 'stop', 'error' ]).forEach(function(status){
-	Engine[ status.toUpperCase() ] = status;
+  Engine[ status.toUpperCase() ] = status;
 });
 
 Engine.prototype._ = _;
@@ -39,208 +39,208 @@ Engine.prototype._ = _;
  * @return {[type]}            [description]
  */
 Engine.prototype.init = function(callback){
-	var engine = this;
-	this.cache = redis.createClient();
-	mongoose.connect(engine.config('db'), function(err, db){
-		callback(engine);
-	});
+  var engine = this;
+  this.cache = redis.createClient();
+  mongoose.connect(engine.config('db'), function(err, db){
+    callback(engine);
+  });
 };
 /**
  * [load description]
  * @return {[type]} [description]
  */
 Engine.prototype.load = function(){
-	var engine = this;
-	//load config file.
-	require(engine.config('config'))(engine);
-	//load crontab file.
-	require(engine.config('crontab'))(engine);
-	//
-	require('./model');
-	//
-	fs.readdir(engine.config('tasks'), function(err, files){
-		files.forEach(function(filename){
-			console.log('loading task: %s', filename.red);
-			require(path.join(engine.root,engine.config('tasks'), filename))(engine);
-		});
-	});
+  var engine = this;
+  //load config file.
+  require(engine.config('config'))(engine);
+  //load crontab file.
+  require(engine.config('crontab'))(engine);
+  //
+  require('./model');
+  //
+  fs.readdir(engine.config('tasks'), function(err, files){
+    files.forEach(function(filename){
+      console.log('loading task: %s', filename.red);
+      require(path.join(engine.root,engine.config('tasks'), filename))(engine);
+    });
+  });
 
-	fs.readdir(engine.config('filters'), function(err, files){
-		files.forEach(function(filename){
-			console.log('loading filter: %s', filename.red);
-			require(path.join(engine.root,engine.config('filters'), filename))(engine);
-		});
-	});
+  fs.readdir(engine.config('filters'), function(err, files){
+    files.forEach(function(filename){
+      console.log('loading filter: %s', filename.red);
+      require(path.join(engine.root,engine.config('filters'), filename))(engine);
+    });
+  });
 };
 
 Engine.prototype.bootstrap = function(){
-	_.each(this.crontab, function(cronjob, name){
-		cronjob.start();
-	})
+  _.each(this.crontab, function(cronjob, name){
+    cronjob.start();
+  })
 };
 
 Engine.prototype.model = function(name){
-	return mongoose.model(name);
+  return mongoose.model(name);
 };
 
 Engine.prototype.cron = function(crontab){
-	var engine = this;
-	for(var job in crontab){
-		(function(key){
-			var job = crontab[ key ];
-			var cronjob = new cron.CronJob(job, function(){
-				if(engine.status(key) != Engine.RUNNING){
-					console.log('cron: %s starting ', key.green);
-					engine.run(key);
-				}else{
-					// console.log('%s already running'.red, key);
-				}
-			}, engine.noop, false, '');
-			engine.crontab[ key ] = cronjob;
-		})(job);
-	}
+  var engine = this;
+  for(var job in crontab){
+    (function(key){
+      var job = crontab[ key ];
+      var cronjob = new cron.CronJob(job, function(){
+        if(engine.status(key) != Engine.RUNNING){
+          console.log('cron: %s starting ', key.green);
+          engine.run(key);
+        }else{
+          // console.log('%s already running'.red, key);
+        }
+      }, engine.noop, false, '');
+      engine.crontab[ key ] = cronjob;
+    })(job);
+  }
 };
 
 Engine.prototype.config = function(key, val){
-	if(typeof key == 'function'){
-		var env = process.env.NODE_ENV || 'development';
-		this.configs = _.extend(this.configs, key(env));
-	}else{
-		if(val){
-			this.configs[key] = val;
-		}else{
-			return this.configs[key];
-		}
-	}
+  if(typeof key == 'function'){
+    var env = process.env.NODE_ENV || 'development';
+    this.configs = _.extend(this.configs, key(env));
+  }else{
+    if(val){
+      this.configs[key] = val;
+    }else{
+      return this.configs[key];
+    }
+  }
 };
 
 Engine.prototype.require = function(requirement_list, callback){
-	var engine = this;
-	var requirements = [];
-	requirement_list.forEach(function(requirement_name){
-		requirements.push( require('./' + requirement_name)(engine) );
-	});
-	callback.apply(callback, requirements);
+  var engine = this;
+  var requirements = [];
+  requirement_list.forEach(function(requirement_name){
+    requirements.push( require('./' + requirement_name)(engine) );
+  });
+  callback.apply(callback, requirements);
 };
 
 
 Engine.prototype.async = function(tasks){
-	return {
-		parallel: function(callback){
-			async.parallel(tasks, callback)
-		},
-		series: function(callback){
-			async.series(tasks, callback);
-		}
-	};
+  return {
+    parallel: function(callback){
+      async.parallel(tasks, callback)
+    },
+    series: function(callback){
+      async.series(tasks, callback);
+    }
+  };
 };
 
 Engine.prototype.noop = function(callback){};
 
 Engine.prototype.promise = function(callback){
-	return new Promise(callback)
+  return new Promise(callback)
 };
 
 Engine.prototype.task = function(name, deps, func){
-	if(typeof deps == 'function'){
-		func = deps;
-		deps = [];
-	}
-	this.tasks[ name ] = {
-		pre: deps,
-		exports: func,
-		status: Engine.STOP
-	};
+  if(typeof deps == 'function'){
+    func = deps;
+    deps = [];
+  }
+  this.tasks[ name ] = {
+    pre: deps,
+    exports: func,
+    status: Engine.STOP
+  };
 };
 
 Engine.prototype.status = function(name){
-	var task = this.tasks[ name ];
-	if(!task){
-		throw new Error('can not found task : ' + name);
-	}
-	return task.status;
+  var task = this.tasks[ name ];
+  if(!task){
+    throw new Error('can not found task : ' + name);
+  }
+  return task.status;
 };
 
 Engine.prototype.run = function(name, callback){
-	var engine = this;
-	var task = engine.tasks[ name ];
-	if(!task){
-		throw new Error('can not found task : ' + name);
-	}
-	task.status = Engine.RUNNING;
-	var preTask = [];
-	task.pre.forEach(function(name){
-		(function(name){
-			preTask.push(function(cb){
-				engine.run(name, cb);
-			});
-		})(name);
-	});
-	engine.async(preTask).series(function(err, results){
-		task.exports(results, callback);
-	});
+  var engine = this;
+  var task = engine.tasks[ name ];
+  if(!task){
+    throw new Error('can not found task : ' + name);
+  }
+  task.status = Engine.RUNNING;
+  var preTask = [];
+  task.pre.forEach(function(name){
+    (function(name){
+      preTask.push(function(cb){
+        engine.run(name, cb);
+      });
+    })(name);
+  });
+  engine.async(preTask).series(function(err, results){
+    task.exports(results, callback);
+  });
 };
 
 Engine.prototype.filter = function(name, exports){
-	this.filters[ name ] = exports;
+  this.filters[ name ] = exports;
 };
 
 Engine.prototype.filterAttribute = function(obj, attrs){
-	if(!obj) return obj;
-	var values = _.map(attrs, function(attr){
-		return obj.hasOwnProperty(attr);
-	});
-	if(_.every(values)) return _.pick(obj, attrs);
+  if(!obj) return obj;
+  var values = _.map(attrs, function(attr){
+    return obj.hasOwnProperty(attr);
+  });
+  if(_.every(values)) return _.pick(obj, attrs);
 };
 
 Engine.prototype.request = function(options, callback){
-	if(typeof options == 'string'){
-		options = { url : options };
-	}
-	var baseUrl = this.config('api');
-	var defaults = {
-		method: 'GET'
-	};
-	options = _.extend(defaults, options);
-	options.url = baseUrl + options.url;
-	var jar = request.jar();
-	var form = request({
-		jar: jar,
-		url: baseUrl + '/login',
-		method: 'POST'
-	}, function(err, res, body){
-			if(err || !body  || res.statusCode != 200){
-				return callback(new Error('login error', err));
-			}
-			options.jar = jar;
-			var req = request(options, function(err, res, body){
-				callback(err, (body && JSON.parse(body)) || body);
-			});
-			req.on('response', function(res){
-				console.log('%s %s',options.method.green, options.url.gray);
-				var length = parseInt(res.headers['content-length'], 10);
-				if(length > (1 * (1024 * 1024)) ){
-					var bar = new ProgressBar('[:bar] :percent :etas', {
-						total: length
-					});
-					req.on('data', function(chunk){
-						bar.tick(chunk.length);
-					});
-				}
-			});
-	}).form();
+  if(typeof options == 'string'){
+    options = { url : options };
+  }
+  var baseUrl = this.config('api');
+  var defaults = {
+    method: 'GET'
+  };
+  options = _.extend(defaults, options);
+  options.url = baseUrl + options.url;
+  var jar = request.jar();
+  var form = request({
+    jar: jar,
+    url: baseUrl + '/login',
+    method: 'POST'
+  }, function(err, res, body){
+      if(err || !body  || res.statusCode != 200){
+        return callback(new Error('login error', err));
+      }
+      options.jar = jar;
+      var req = request(options, function(err, res, body){
+        callback(err, (body && JSON.parse(body)) || body);
+      });
+      req.on('response', function(res){
+        console.log('%s %s',options.method.green, options.url.gray);
+        var length = parseInt(res.headers['content-length'], 10);
+        if(length > (1 * (1024 * 1024)) ){
+          var bar = new ProgressBar('[:bar] :percent :etas', {
+            total: length
+          });
+          req.on('data', function(chunk){
+            bar.tick(chunk.length);
+          });
+        }
+      });
+  }).form();
 
-	form.append('username', 'admin1');
-	form.append('password', 'xiaoshu815');
+  form.append('username', 'admin1');
+  form.append('password', 'xiaoshu815');
 };
 
 
 module.exports = function(options, callback){
-	try{
-		var engine = new Engine(options);
-	}catch(e){
-		throw e;
-	}finally{
-		engine.init(callback);
-	}
+  try{
+    var engine = new Engine(options);
+  }catch(e){
+    throw e;
+  }finally{
+    engine.init(callback);
+  }
 };
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 var express = require('express');
-var logger	=	require('express-log');
-var _				=	require('underscore');
+var logger  = require('express-log');
+var _       = require('underscore');
 
-var redis		=	require('redis');
+var redis   = require('redis');
 var mongodb = require('mongodb');
 
-var async 	=	require('async');
+var async   = require('async');
 
-var cache 	= redis.createClient();
-var client 	= mongodb.MongoClient;
+var cache   = redis.createClient();
+var client  = mongodb.MongoClient;
 
 var app = express();
 
 app.use(logger());
 
 _.mixin({
-	// Return a copy of the object containing all but the blacklisted properties.
-	unpick: function (obj) {
-		return _.pick(obj, _.difference(_.keys(obj), _.flatten(Array.prototype.slice.call(arguments, 1))));
-	}
+  // Return a copy of the object containing all but the blacklisted properties.
+  unpick: function (obj) {
+    return _.pick(obj, _.difference(_.keys(obj), _.flatten(Array.prototype.slice.call(arguments, 1))));
+  }
 });
 
 /**
@@ -27,83 +27,83 @@ _.mixin({
  * @return {[type]}      [description]
  */
 var toChapterify = function(user){
-	var chapterify = {};
-	//group by chapterId
-	for(var eventKey in user.stats){
-		var tracks = user.stats[ eventKey ];
-		tracks.forEach(function(track){
-			chapterify[ track.ChapterId ] = chapterify[ track.ChapterId ] || {};
-			chapterify[ track.ChapterId ][ track.LessonId ] = chapterify[ track.ChapterId ][ track.LessonId ] || {};
-			chapterify[ track.ChapterId ][ track.LessonId ][ eventKey ] = chapterify[ track.ChapterId ][ track.LessonId ][ eventKey ] || [];
-			chapterify[ track.ChapterId ][ track.LessonId ][ eventKey ].push(_.unpick(track, 'LessonId', 'ChapterId' ));
-		});
-	}
-	return chapterify;
+  var chapterify = {};
+  //group by chapterId
+  for(var eventKey in user.stats){
+    var tracks = user.stats[ eventKey ];
+    tracks.forEach(function(track){
+      chapterify[ track.ChapterId ] = chapterify[ track.ChapterId ] || {};
+      chapterify[ track.ChapterId ][ track.LessonId ] = chapterify[ track.ChapterId ][ track.LessonId ] || {};
+      chapterify[ track.ChapterId ][ track.LessonId ][ eventKey ] = chapterify[ track.ChapterId ][ track.LessonId ][ eventKey ] || [];
+      chapterify[ track.ChapterId ][ track.LessonId ][ eventKey ].push(_.unpick(track, 'LessonId', 'ChapterId' ));
+    });
+  }
+  return chapterify;
 };
 
 var individuals = function(chapterId, roomId, callback){
-	var db = app.get('db');
-	var Record = db.collection('records');
-	cache.hkeys('room:' + roomId, function(err, students){
-		var taskGroup = [];
-		students.forEach(function(userId){
-			taskGroup.push(function(callback){
-				Record.findOne({ 'user_id': userId }, function(err, user){
-					var stat;
-					if(!err && user){
-						var chapterify = toChapterify(user);
-						if(chapterify[ chapterId ]) {
-							stat = {
-								userId: userId,
-								stats: (chapterify[ chapterId ])
-							};
-						}
-					}
-					callback(err, stat);
-				});
-			});
-		});
-		async.parallel(taskGroup, function(err, results){
-			callback(err, _.compact(results));
-		});
+  var db = app.get('db');
+  var Record = db.collection('records');
+  cache.hkeys('room:' + roomId, function(err, students){
+    var taskGroup = [];
+    students.forEach(function(userId){
+      taskGroup.push(function(callback){
+        Record.findOne({ 'user_id': userId }, function(err, user){
+          var stat;
+          if(!err && user){
+            var chapterify = toChapterify(user);
+            if(chapterify[ chapterId ]) {
+              stat = {
+                userId: userId,
+                stats: (chapterify[ chapterId ])
+              };
+            }
+          }
+          callback(err, stat);
+        });
+      });
+    });
+    async.parallel(taskGroup, function(err, results){
+      callback(err, _.compact(results));
+    });
 
-	});
+  });
 };
 
 
 app.get('/stats/individuals', function(req, res){
-	var chapterId = req.query['chapterId'];
-	var roomId 		= req.query['roomId'];
-	individuals(chapterId, roomId, function(err, results){
-		res.send(results);
-	});
+  var chapterId = req.query['chapterId'];
+  var roomId    = req.query['roomId'];
+  individuals(chapterId, roomId, function(err, results){
+    res.send(results);
+  });
 });
 
 app.get('/stats/rooms', function(req, res){
-	var chapterId = req.query['chapterId'];
-	var roomId 		= req.query['roomId'];
-	individuals(chapterId, roomId, function(err, results){
-		var lessonify = {};
-		results.forEach(function(user){
-			for(var lessonId in user.stats){
-				lessonify[ lessonId ] = lessonify[ lessonId ] || {};
-				var stats = user.stats[lessonId];
-				for(var eventKey in stats){
-					var tracks = stats[ eventKey ];
-					lessonify[ lessonId ][ eventKey ] = lessonify[ lessonId ][ eventKey ] || [];
-					tracks.forEach(function(track){
-						track.userId = user.userId;
-						lessonify[ lessonId ][ eventKey ].push(track);
-					});
-				}
-			}
-		});
-		res.send(lessonify);
-	});
+  var chapterId = req.query['chapterId'];
+  var roomId    = req.query['roomId'];
+  individuals(chapterId, roomId, function(err, results){
+    var lessonify = {};
+    results.forEach(function(user){
+      for(var lessonId in user.stats){
+        lessonify[ lessonId ] = lessonify[ lessonId ] || {};
+        var stats = user.stats[lessonId];
+        for(var eventKey in stats){
+          var tracks = stats[ eventKey ];
+          lessonify[ lessonId ][ eventKey ] = lessonify[ lessonId ][ eventKey ] || [];
+          tracks.forEach(function(track){
+            track.userId = user.userId;
+            lessonify[ lessonId ][ eventKey ].push(track);
+          });
+        }
+      }
+    });
+    res.send(lessonify);
+  });
 });
 
 
 client.connect('mongodb://localhost/teacher-dashboard', function(err, db){
-	app.set('db', db);
-	app.listen(3002);
+  app.set('db', db);
+  app.listen(3002);
 });
